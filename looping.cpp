6fb3425b516b831980b28ce27b42cc0482664dc6
annisafathul <annisafathul18@gmail.com>
2026-03-30 11:56:42 +0700

    Melakukan perulangan

--- looping.cpp
+++ looping.cpp
@@ -27,6 +27,8 @@ cout<<endl;
     }
      cout<<"Bilangan acak while yang terakhir = "<<x<<endl;
     cout<<endl;
-    
+    //Perulangan dengan DO...WHILE
+    cout<<"PERULANGAN DO...WHILE"<<endl;
+    x = 1 + rand() % 10;
 
     
