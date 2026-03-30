f7af7a383b2644dd370c11f42e689b459c7fd47f
annisafathul <annisafathul18@gmail.com>
2026-03-30 12:15:31 +0700

    pilihan jurusan

--- switchCase.cpp
+++ switchCase.cpp
@@ -18,3 +18,19 @@ void input() {
 // prosedur untuk pengecekan kondisi
 void pilihanJurusan() {
     switch (kodeJurusan) {
+        case 1:
+            namaJurusan = "Teknologi Informasi";
+            statusKuota = "Tersedia (15 Kursi)";
+            break;
+        case 2:
+            namaJurusan = "Teknologi Elektro";
+            statusKuota = "Kuota Penuh!";
+            break;
+        case 3:
+            namaJurusan = "Sistem Informasi";
+            statusKuota = "Tersedia (5 Kursi)";
+            break;
+        default:
+            namaJurusan = "Tidak Diketahui";
+            statusKuota = "Error: Kode Tidak Valid";
+    }
