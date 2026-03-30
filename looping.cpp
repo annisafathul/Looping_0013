d2aed99e6f2ece5f00a8f2280bcd5c248cb78b0c
annisafathul <annisafathul18@gmail.com>
2026-03-30 12:27:52 +0700

    logika OR

--- looping.cpp
+++ looping.cpp
@@ -58,4 +58,14 @@ rerata = (nilB + nilM) / 2;
     cout<<"Status kelulusan = "<<status
         <<", dengan nilai rata-rata = "<<rerata<<endl;
 }
-    
+    #include <iostream>
+using namespace std;
+
+int main(){
+    float nilB, nilM, rerata;
+    string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
