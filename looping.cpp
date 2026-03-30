c5a44e731f5be2d1898e99e6e7e01f267e3fcfa4
annisafathul <annisafathul18@gmail.com>
2026-03-30 12:30:13 +0700

    logika AND

--- looping.cpp
+++ looping.cpp
@@ -79,3 +79,9 @@ int main(){
     cout<<"Status kelulusan = "<<status
         <<", dengan nilai rata-rata = "<<rerata<<endl;
 }
+#include <iostream>
+using namespace std;
+
+int main(){
+    float nilB, nilM, rerata;
+    string status;
